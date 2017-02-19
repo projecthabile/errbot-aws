d088f9adb786d59f94f3f0f05e9351578edabc5c
Arun Dalal <arundalal786@gmail.com>
2017-02-19 08:21:32 +0000

    listid command added

---
+++ aws.py
@@ -0,0 +1,24 @@
+from errbot import BotPlugin, botcmd
+from optparse import OptionParser
+
+import logging
+import socket
+import time
+import boto3
+log = logging.getLogger(name='errbot.plugins.AWS')
+
+class AWS(BotPlugin):
+    
+    
+    @botcmd(split_args_with=None)
+    def ec2_listid(self, msg, args):
+        '''list all ec2 instances in account
+           option: None
+           !ec2 listid
+        '''
+        idlist = []
+        ec2 = boto3.resource('ec2')
+        for instance in ec2.instances.all():
+            idlist.append(instance.id)
+            
+        return idlist
